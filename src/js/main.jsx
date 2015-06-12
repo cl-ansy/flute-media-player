b3e98163c20fecd9399ce49dbc28c7fed0f2d744
Chris Lansing <chris.lansing@prometheanworld.com>
2015-06-12 11:42:17 -0400

    adding basic styling

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import Aside from './components/aside';
-import Section from './components/section';
+import React    from 'react';
+
+import Aside    from './components/aside';
+import Section  from './components/section';
 
 var mountNode = document.getElementById('main');
+
+document.documentElement.style.height = '100%';
+document.body.style.height = '100%';
+document.body.style.margin = '0';
+mountNode.style.height = '100%';
+
+var styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '100%'
+    }
+};
 
 class Main extends React.Component {
     render() {
         return (
-            <div className='container'>
+            <div className='container' style={styles.container}>
                 <Aside />
                 <Section />
             </div>
