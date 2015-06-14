db1ce959e8d5ce046262766e86f3adeea204d7d4
Chris Lansing <cslansin@gmail.com>
2015-06-14 04:25:27 -0400

    default header title

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -4,10 +4,11 @@ class Header extends React.Component {
     render() {
         return (
             <header className='comp-header'>
-                <button className='btn pure-button'
+                <button
+                    className='btn pure-button'
                     onClick={this.props.handleNavToggle}>
                     <i className='fa fa-bars'></i></button>
-                <h2>{this.props.selectedFile.name}</h2>
+                <h2>{this.props.selectedFile.name || 'Flute Media Player'}</h2>
             </header>
         );
     }
